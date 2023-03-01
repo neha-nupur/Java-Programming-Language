c59a229e987a79c55fe2d8fc99d4ae83a808d035
Neha Nupur <110279038+neha-nupur@users.noreply.github.com>
2023-03-01 22:23:58 +0530

    Update ToUpperCase.java

--- Strings/ToUpperCase.java
+++ Strings/ToUpperCase.java
@@ -6,7 +6,7 @@ public class ToUpperCase {
 
         sb.append(ch);
 
-        for(int i=0; i<str.length(); i++) {
+        for(int i=1; i<str.length(); i++) {
             if(str.charAt(i) == ' ' && i < str.length()-1) {
                 sb.append(str.charAt(i));
                 i++;
