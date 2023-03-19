21fb9de224e563aa18a1d4b90b66cbf311b6ef1b
Neha Nupur <110279038+neha-nupur@users.noreply.github.com>
2023-03-19 20:54:11 +0530

    Folder Binary Search Added

---
+++ Binary Search/SortedRotatedArraySearch.java
@@ -0,0 +1,49 @@
+public class SortedRotatedArraySearch {
+
+    public static int search(int arr[], int tar, int si, int ei) {
+        if(si > ei) {
+            //invalid condition
+            return -1;  // cannot found target at any index
+        }
+
+
+        // Conditions- Works to do
+        int mid = si + (ei - si) / 2;
+
+        // case Found 
+        if(arr[mid] == tar) {
+            return mid;
+        }
+
+        // Case 1 - mid on L1
+        if(arr[si] <= arr[mid]) {
+            // case a: left
+            if(arr[si]<= tar && tar<= arr[mid]) {
+                return search(arr, tar, si, mid-1);
+            } else {
+                // case b: right
+                return search(arr, tar, mid+1, ei);
+            }
+        }
+
+
+        // Case 2 - mid on L2
+        else {
+            //case a: right
+            if(arr[mid] <= tar && tar<= arr[ei]) {
+                return search(arr, tar, mid+1, ei);
+            } else {
+                //case b: left
+                return search(arr, tar, si, mid-1);
+            }
+        }
+    }
+
+
+    public static void main(String args[]) {
+        int arr[] = {4, 5, 6, 7, 0, 1, 2};
+        int target = 6; 
+        int tarIdx = search(arr, target, 0, arr.length-1);
+        System.out.println(tarIdx);
+    }
+}
